44386f641697e7b7cf7b26c68b248e5bea62b888
Agata Sumowska <agata.sumowska@gmail.com>
2025-07-23 13:01:39 +0200

    Add CORS configuration support

--- api_gateway.tf
+++ api_gateway.tf
@@ -6,4 +6,17 @@ resource "aws_apigatewayv2_api" "api_gateway" {
   disable_execute_api_endpoint = !local.enable_execute_api_endpoint
 
   tags = local.tags
+
+  dynamic "cors_configuration" {
+    for_each = var.cors_enabled ? [1] : []
+    content {
+      allow_origins     = var.cors_allow_origins
+      allow_methods     = var.cors_allow_methods
+      allow_headers     = var.cors_allow_headers
+      max_age           = var.cors_max_age
+      allow_credentials = var.cors_allow_credentials
+      expose_headers    = var.cors_expose_headers
+    }
+  }
 }
+
--- examples/full/variables.tf
+++ examples/full/variables.tf
@@ -5,3 +5,38 @@ variable "deployment_identifier" {}
 
 variable "domain_name" {}
 variable "public_zone_id" {}
+
+variable "cors_enabled" {
+  description = "Enable CORS on the API Gateway"
+  type        = bool
+}
+
+variable "cors_allow_origins" {
+  description = "List of allowed origins for CORS"
+  type        = list(string)
+}
+
+variable "cors_allow_methods" {
+  description = "List of allowed HTTP methods for CORS"
+  type        = list(string)
+}
+
+variable "cors_allow_headers" {
+  description = "List of allowed headers for CORS"
+  type        = list(string)
+}
+
+variable "cors_max_age" {
+  description = "CORS max age in seconds"
+  type        = number
+}
+
+variable "cors_allow_credentials" {
+  description = "Whether to allow credentials for CORS"
+  type        = bool
+}
+
+variable "cors_expose_headers" {
+  description = "List of exposed headers for CORS"
+  type        = list(string)
+}
